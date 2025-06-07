506743b8330c5609b82282eee213a1a44b5ebf82
leodev6 <djouakalionel4@gmail.com>
2025-06-07 16:00:16 +0200

    Premier commit du project - Panier API - Application de Gestion de Panier d'Achat

---
+++ src/main/java/it/codecrafteur/gestiondeminiecomerce/GestiondeminiecomerceApplication.java
@@ -0,0 +1,13 @@
+package it.codecrafteur.gestiondeminiecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondeminiecomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondeminiecomerceApplication.class, args);
+	}
+
+}
