56aeccf717d01bd389d83c42b4b8a0a5ca51c8ca
leodev6 <djouakalionel4@gmail.com>
2025-06-07 17:35:41 +0200

    Creation de l'entité CartItem

--- src/main/java/it/codecrafteur/gestiondeminiecomerce/entity/CartItem.java
+++ src/main/java/it/codecrafteur/gestiondeminiecomerce/entity/CartItem.java
@@ -1,20 +1,107 @@
 package it.codecrafteur.gestiondeminiecomerce.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "cart_items")
 public class CartItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "L'ID du produit est obligatoire")
+    @Column(name = "product_id", nullable = false)
     private Long productId;
 
+    @NotNull(message = "Le nom du produit est obligatoire")
+    @Column(name = "produit_name", nullable = false)
     private String productName;
 
+    @NotNull(message = "Le prix unitaire est obligatoire")
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
 
+    @NotNull(message = "La quantité est obligatoire")
+    @Min(value = 1, message = "La quantité doit etre au moins de 1")
+    @Column(nullable = false)
     private Integer quantity;
 
+    @NotNull(message = "Le prix est obligatoire")
+    @Column(name = "total_price", nullable = false, precision = 10, scale = 2)
     private BigDecimal totalPrice;
 
+    public CartItem(){}
+    public CartItem(Long id, Long productId, String productName, BigDecimal unitPrice, Integer quantity, BigDecimal totalPrice) {
+        this.id = id;
+        this.productId = productId;
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
 }
